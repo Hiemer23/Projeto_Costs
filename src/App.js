f399bd6286fcec73d03a4a4c44e3272e879df1fd
Andrey Hiemer <100521047+Hiemer23@users.noreply.github.com>
2022-12-20 20:39:33 -0300

    Inicio do Projeto

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewProject from "./components/pages/NewProject";
+
+import Container from "./components/layout/Container";
+
+function App() {
+  return (
+
+    <Router>
+      <div>
+        <Link to='/'>Home</Link>
+        <Link to='/company'>Company</Link>
+        <Link to='/contact'>Contact</Link>
+        <Link to='/newproject'>NewProject</Link>
+      </div>
+      <Container customClass='min-height'>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route path="/company" element={<Company />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/newproject" element={<NewProject />}></Route>
+        </Routes>
+      </Container>
+      <p>Footer</p>
+    </Router>
+  )
+}
+
+export default App;
