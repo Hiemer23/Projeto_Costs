ca6d2327286943eb312e717daf29c613a88f4738
Hiemer23 <andreyhiemer23@gmail.com>
2022-12-21 11:06:41 -0300

    Criação de API para consultar o banco de dados

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from "./components/pages/Home";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
@@ -20,6 +20,7 @@ function App() {
           <Route path="/company" element={<Company />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
           <Route path="/projects" element={<Projects />}></Route>
+          <Route path="/newproject" element={<NewProject />}></Route>
         </Routes>
       </Container>
       <Footer />
