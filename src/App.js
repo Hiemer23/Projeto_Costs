6449515b90358262a9dabd655990166a99071b23
Andrey Hiemer <100521047+Hiemer23@users.noreply.github.com>
2022-12-20 21:38:10 -0300

    Atualização

--- src/App.js
+++ src/App.js
@@ -6,17 +6,13 @@ import Contact from "./components/pages/Contact";
 import NewProject from "./components/pages/NewProject";
 
 import Container from "./components/layout/Container";
-
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 function App() {
   return (
 
     <Router>
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/company'>Company</Link>
-        <Link to='/contact'>Contact</Link>
-        <Link to='/newproject'>NewProject</Link>
-      </div>
+      <Navbar />
       <Container customClass='min-height'>
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
@@ -25,7 +21,7 @@ function App() {
           <Route path="/newproject" element={<NewProject />}></Route>
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer />
     </Router>
   )
 }
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,4 @@
+function Footer(){
+    return(<dix></dix>)
+}
+export default Footer
